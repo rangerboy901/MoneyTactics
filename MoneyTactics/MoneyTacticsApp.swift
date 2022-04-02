482c986d2e45f89bb861ec24a69a01a775ee6248
MojoMoogly574 <jdweese574@gmail.com>
2022-04-02 11:02:26 -0500

    Initial Commit

---
+++ MoneyTactics/MoneyTacticsApp.swift
@@ -0,0 +1,20 @@
+//
+//  MoneyTacticsApp.swift
+//  MoneyTactics
+//
+//  Created by Joseph DeWeese on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTacticsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
